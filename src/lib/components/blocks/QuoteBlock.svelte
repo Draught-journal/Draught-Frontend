6551be6d606fd645965b4273079732378472d591
Alvin Ashiatey <alvinashiatey@gmail.com>
2025-09-11 13:45:13 -0400

    Refactor QuoteBlock component to improve text structure and styling; remove citation display and adjust layout for better readability

--- src/lib/components/blocks/QuoteBlock.svelte
+++ src/lib/components/blocks/QuoteBlock.svelte
@@ -6,31 +6,31 @@
 
 <div class="quote-block" data-block-id={content.id}>
 	<blockquote class="quote-content">
-		<p class="quote-text">"{content.content.text}"</p>
-		<cite class="quote-citation">— {content.content.citation}</cite>
+		<p class="quote-text">{content.content.text}</p>
+		<p class="quote-text">
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione aperiam amet magnam
+			necessitatibus maxime nobis ipsa, sequi, nam hic molestias rem, distinctio sint aliquam
+			aspernatur rerum est voluptate aut. Totam! Lorem ipsum dolor sit amet consectetur adipisicing
+			elit. Totam exercitationem dolor magni magnam fuga possimus sapiente, quasi excepturi, ipsa
+			facere facilis, officia voluptate incidunt culpa sed quod delectus explicabo deleniti.
+		</p>
 	</blockquote>
 </div>
 
 <style>
 	.quote-block {
+		margin: 0 auto;
 		margin-bottom: 2rem;
+		width: 90%;
 	}
 
 	.quote-content {
 		margin: 0;
-		text-indent: 1rem;
 	}
 
 	.quote-text {
 		margin: 0 0 1rem 0;
-		font-size: 1.1em;
-		line-height: 1.6;
-	}
-
-	.quote-citation {
-		display: block;
-		margin: 0;
-		font-style: normal;
+		line-height: 23px;
 	}
 
 	/* Responsive adjustments */
@@ -45,10 +45,6 @@
 
 		.quote-text {
 			font-size: 1em;
-		}
-
-		.quote-citation {
-			font-size: 0.8em;
 		}
 	}
 </style>
