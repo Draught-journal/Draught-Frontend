07358c85877a7377d24692ad540d527c4cdb4c15
Alvin Ashiatey <alvinashiatey@gmail.com>
2025-09-11 13:45:39 -0400

    Remove placeholder text from QuoteBlock component to streamline content display

--- src/lib/components/blocks/QuoteBlock.svelte
+++ src/lib/components/blocks/QuoteBlock.svelte
@@ -7,13 +7,6 @@
 <div class="quote-block" data-block-id={content.id}>
 	<blockquote class="quote-content">
 		<p class="quote-text">{content.content.text}</p>
-		<p class="quote-text">
-			Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione aperiam amet magnam
-			necessitatibus maxime nobis ipsa, sequi, nam hic molestias rem, distinctio sint aliquam
-			aspernatur rerum est voluptate aut. Totam! Lorem ipsum dolor sit amet consectetur adipisicing
-			elit. Totam exercitationem dolor magni magnam fuga possimus sapiente, quasi excepturi, ipsa
-			facere facilis, officia voluptate incidunt culpa sed quod delectus explicabo deleniti.
-		</p>
 	</blockquote>
 </div>
 
